ec216aab488e8f6c8ca79957bd5af588c889acd9
IvinFrancisV <ivinfrancisv@gmail.com>
2023-10-04 14:05:54 +0530

    Added Modifications

--- LiveData_App/urls.py
+++ LiveData_App/urls.py
@@ -3,4 +3,6 @@ from LiveData_App import views
 
 urlpatterns = [
     path('', views.sample, name='sample'),
+    path('view_about', views.view_about, name='view_about'),
+    
 ]
--- LiveData_App/views.py
+++ LiveData_App/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def sample(request):
     return render(request, "sample.html")
+
+def view_about(request):
+    return render(request, "about.html")
